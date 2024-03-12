99984fa8994077015af0696c456c2d2c2d974acb
johnpetros <joaopedro.nc@outlook.com>
2024-03-11 22:49:15 -0300

    ✨ feat (site): add HomePageView

--- src/http/modules/ui/views/HomePageView.tsx
+++ src/http/modules/ui/views/HomePageView.tsx
@@ -6,8 +6,14 @@ import { Home } from 'site/pages/Home'
 
 export class HomePageView implements IView {
   async render(http: IHttp): Promise<string> {
+    const clipboardScript = await Bun.file(
+      './node_modules/clipboard/dist/clipboard.min.js'
+    ).text()
+
+    const prelineScript = await Bun.file('./node_modules/preline/dist/preline.js').text()
+
     return http.renderView(
-      <RootLayout>
+      <RootLayout scripts={[clipboardScript, prelineScript]}>
         <Home />
       </RootLayout>
     )
