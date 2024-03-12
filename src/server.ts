d7d14ef229c55e7cbf6fe0e6e8f6cd2043fb93a2
johnpetros <joaopedro.nc@outlook.com>
2024-03-12 19:08:09 -0300

    📦 build: add scripts to start app

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import { app } from 'app'
 
 const PORT = Number(process.env.PORT) ?? 3000
 
-app.startServer(PORT, () => console.log('Server is running on port: ' + PORT))
+app.startServer(PORT, () => console.log(`Server is running on port: ${PORT}`))
