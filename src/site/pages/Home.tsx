4ffcfafdc4174c2b15f21edbd6ead400a6ccb9a8
johnpetros <joaopedro.nc@outlook.com>
2024-03-11 22:52:33 -0300

    ✨ feat (site): add Home page

---
+++ src/site/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { ClipboardButton } from '@site/components/ClipboardButton'
+
+export function Home() {
+  return (
+    <main class='bg-neutral-900 h-auto px-6 lg:px-0 pb-6'>
+      <header class='flex items-center justify-end h-24'>
+        <a href='/' target='_blank' rel='noreferrer'>
+          <img src='https://usehooks.com/img/icon-github.svg' alt='' />
+        </a>
+      </header>
+      <h1 class='text-center text-4xl text-neutral-300 italic'>
+        HTTP
+        <span class='ml-2 text-pink-400 text-5xl font-extrabold'>Pandas</span>
+      </h1>
+      <p class='text-neutral-300 text-center mt-12 text-2xl'>
+        Cute Pandas for Each HTTP Response Status Code.
+      </p>
+
+      <div class='flex itens-center justify-between mx-auto rounded-md bg-neutral-800 w-full max-w-[40rem] mt-8 p-4 text-lg'>
+        <span class='block py-2 text-neutral-300 text-sm sm:text-base'>
+          https://http.pandas/[status_code]
+        </span>
+        <ClipboardButton />
+      </div>
+
+      <div hx-get='/ui/pandas' hx-trigger='load' hx-swap='outerHTML' />
+    </main>
+  )
+}
